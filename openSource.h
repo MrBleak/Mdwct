0668f43fed7878630106cde80906f5dac3ca21d5
MrZrh <1039369602@qq.com>
2017-08-17 20:49:11 +0800

    zhangrenhe add openSource 2017年 08月 17日 星期四 20:49:47 CST

---
+++ openSource.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: openSource.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年08月17日 星期四 20时47分56秒
+ ************************************************************************/
+
+#ifndef _OPENSOURCE_H
+#define _OPENSOURCE_H
+
+#include <stdio.h>
+#include <string.h>
+
+#endif
