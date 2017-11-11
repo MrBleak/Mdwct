fd31cac8b10f93cf787a55c6891595f379f47713
Zrh-Ubuntu <zhangrenhe7481@163.com>
2017-11-11 15:27:08 +0800

    zhangrenhe add

--- bin/getSource_bak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find ./ -name *.c > SourcePathbak
-
-find ./ -name *.cpp >> SourcePathbak
-find ./ -name *.cxx >> SourcePathbak
-find ./ -name *.cc >> SourcePathbak
-find ./ -name *.C >> SourcePathbak
-find ./ -name *.c++ >> SourcePathbak
